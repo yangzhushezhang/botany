f83817b6d5e7190d71773ff83567861c13152156
wangyi <wangyi123>
2021-10-28 17:28:07 +0800

    2021年10月28日17:28:06

--- App/Task/SpecialSeedTask.php
+++ App/Task/SpecialSeedTask.php
@@ -56,6 +56,7 @@ class SpecialSeedTask implements TaskInterface
             $client_http->setHeaders($headers, false, false);
             $client_http->setTimeout(5);
             $client_http->setConnectTimeout(10);
+
             $data = '{"landId":0,"plantId":' . $this->data['plantId'] . '}';
             $response = $client_http->post($data);
             $result = $response->getBody();
@@ -91,22 +92,52 @@ class SpecialSeedTask implements TaskInterface
 
                 Tools::WriteLogger($this->data['user_id'], 1, '特殊种子种植成功 ' . $result, $this->data['account_number_id'], 2);
 
-
                 #先购买大花盘
                 $one = AccountNumberModel::create()->get(['id' => $this->data['account_number_id']]);
                 $leWallet = $one['leWallet'];
                 $result = Tools::shoppingTools(2, $this->data['token_value'], $data_json['data']['_id'], $this->data['account_number_id'], $this->data['user_id'], $leWallet);#($id, $token_value, $user_id, $account_number_id, $leWallet)
                 if (!$result) {
-                    #G 购买成功  大花
+                    #G 购买大花盆 失败
                     Tools::WriteLogger($this->data['user_id'], 2, "为特殊种子购买大的花盆失败", $this->data['account_number_id'], 2, $data_json['data']['_id']);
                     return false;  #到此为止
 
                 }
 
+                #放大花盆
+                for ($j = 0; $j < 5; $j++) {
+                    $client_http = new \EasySwoole\HttpClient\HttpClient('https://backend-farm.plantvsundead.com/farms/apply-tool');
+                    $headers = array(
+                        'authority' => 'backend-farm.plantvsundead.com',
+                        'sec-ch-ua' => '"Google Chrome";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+                        'accept' => 'application/json, text/plain, */*',
+                        'content-type' => 'application/json;charset=UTF-8',
+                        'authorization' => $this->data['token_value'],
+                        'sec-ch-ua-mobile' => '?0',
+                        'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+                        'sec-ch-ua-platform' => '"Windows"',
+                        'origin' => 'https://marketplace.plantvsundead.com',
+                        'sec-fetch-site' => 'same-site',
+                        'sec-fetch-mode' => 'cors',
+                        'sec-fetch-dest' => 'empty',
+                        'referer' => 'https://marketplace.plantvsundead.com/',
+                        'accept-language' => 'zh-CN,zh;q=0.9',
+                    );
+                    $client_http->setHeaders($headers, false, false);
+                    $data = '{"farmId":"' . $data_json['data']['_id'] . '","toolId":2,"token":{"challenge":"default","seccode":"default","validate":"default"}}';
+                    $client_http->setTimeout(5);
+                    $client_http->setConnectTimeout(10);
+                    $response = $client_http->post($data);
+                    $response = $response->getBody();
+                    $data_json = json_decode($response, true);
+                    if ($data_json && $data_json['status'] == 0) {
+                        #推入浇水 进程
+                        $redis->rPush("Watering", $res . "@" . $this->data['account_number_id'] . "@" . $this->data['user_id']);  # 农场id 账户id  用户 id
+                        Tools::WriteLogger($this->data['user_id'], 2, "进程 PutPotProcess 放大花盆成功,并推入 WateringProcess 进程 First result:" . $response, $this->data['account_number_id'], 2, $data_json['data']['_id']);
 
-                #推入浇水 进程
-                $redis->rPush("Watering", $res . "@" . $this->data['account_number_id'] . "@" . $this->data['user_id']);  # 农场id 账户id  用户 id
-                Tools::WriteLogger($this->data['user_id'], 2, "进程 PutPotProcess 放大花盆成功,并推入 WateringProcess 进程 First result:" . $response, $this->data['account_number_id'], 2, $data_json['data']['_id']);
+                    }
+
+                }
+                Tools::WriteLogger($this->data['user_id'], 2, "进程 PutPotProcess 放大花盆失败" . $response, $this->data['account_number_id'], 2, $data_json['data']['_id']);
                 return true;
             }
         }
